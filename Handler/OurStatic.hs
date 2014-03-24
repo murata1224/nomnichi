ebb17725d0ff229e21e52578c6356cd8db28597c
Hatsune Kitagawa <mugi.kitten@gmail.com>
2014-03-24 17:32:50 +0900

    Try and error

--- Handler/OurStatic.hs
+++ Handler/OurStatic.hs
@@ -6,19 +6,23 @@ where
 import Import
 import Data.Time
 import Data.List
+import qualified Data.Text as DT
 import System.Locale (defaultTimeLocale)
 import System.IO.Unsafe (unsafePerformIO) 
 import System.IO (readFile)
 import Text.Blaze.Html (preEscapedToHtml)
-
+import Data.ByteString.Char8 (unpack)
 import Yesod.Form.Nic (YesodNic, nicHtmlField)
 instance YesodNic App
 
 readStaticFile :: FilePath -> Html
 readStaticFile filePath = preEscapedToHtml $ unsafePerformIO $ readFile filePath
 
-getOurStaticR :: String -> Handler Html
+textsToFilePath :: [Text] -> FilePath
+textsToFilePath texts = "public_html/" ++ ( init $ tail $ show $ DT.intercalate "/" texts )
+
+getOurStaticR :: [Text] -> Handler Html
 getOurStaticR path = do
     defaultLayout $ do
-      [whamlet|#{readStaticFile ("public_html/" ++ path)}|]
+      [whamlet|#{readStaticFile $ textsToFilePath path}|]
 
